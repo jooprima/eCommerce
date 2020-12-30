269487d55acf730036620b13fc9c10168aeaf78b
jooprima <jooprima@gmail.com>
2020-12-30 13:14:03 +0700

    ProductCard component first version

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,32 @@
 import Layout from "@common/components/Layout";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import ProductCard from "@common/components/ProductCard";
 
 const HomePage = ({ products }) => {
   const { list } = products;
 
   return (
     <Layout>
-      <div>
-        {list.map((list) => {
-          return <p>{list.name}</p>;
-        })}
-      </div>
+      <Container maxWidth="sm">
+        <Grid container spacing={2}>
+          {list.map((list) => {
+            return (
+              <Grid key={list.id} item xs={6} sm={6}>
+                <ProductCard
+                  productID={list.id}
+                  img={list.img}
+                  title={list.name}
+                  promoLabel={list.promo}
+                  price={list.price}
+                  rating={list.rating}
+                  sold={list.sold}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
     </Layout>
   );
 };
