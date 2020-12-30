0a5d0f30719abdc211a7c7046f64954c42d9b4ad
jooprima <jooprima@gmail.com>
2020-12-31 01:43:17 +0700

    Modify ProductCard with Link

--- modules/common/components/ProductCard/index.js
+++ modules/common/components/ProductCard/index.js
@@ -44,27 +44,29 @@ const ProductCard = ({
 }) => {
   const classes = useStyles();
   return (
-    <Card>
-      <div className={classes.card}>
-        <div className={classes.relative}>
-          <CardMedia className={classes.image} image={img} title={title} />
-          <div className={classes.promoLabel}>
-            <PromoLabel promoLabel={promoLabel} />
+    <Link href="product/[id]" as={`product/${productID}`}>
+      <Card>
+        <div className={classes.card}>
+          <div className={classes.relative}>
+            <CardMedia className={classes.image} image={img} title={title} />
+            <div className={classes.promoLabel}>
+              <PromoLabel promoLabel={promoLabel} />
+            </div>
           </div>
+          <CardContent>
+            <Grid container direction="column">
+              <Typography gutterBottom variant="subtitle2" component="h2">
+                {title}
+              </Typography>
+              <Typography variant="overline" className={classes.price}>
+                {currencyFormatter(price)}
+              </Typography>
+              <ProductRating rating={rating} sold={sold} />
+            </Grid>
+          </CardContent>
         </div>
-        <CardContent>
-          <Grid container direction="column">
-            <Typography gutterBottom variant="subtitle2" component="h2">
-              {title}
-            </Typography>
-            <Typography variant="overline" className={classes.price}>
-              {currencyFormatter(price)}
-            </Typography>
-            <ProductRating rating={rating} sold={sold} />
-          </Grid>
-        </CardContent>
-      </div>
-    </Card>
+      </Card>
+    </Link>
   );
 };
 
